00233c42d2ec1d91749cf67db9c50965535ff6af
Dan Whitehouse <drw@dim-sum.org>
2023-07-31 10:09:19 +0100

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Dan Whitehouse on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
